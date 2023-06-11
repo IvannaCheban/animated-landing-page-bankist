979df8cf789060ad932951f0f802028ee21c370c
IvannaCheban <ivanna_cheban@ukr.net>
2023-06-11 14:07:26 +0300

    add and remove cookies message

--- script.js
+++ script.js
@@ -28,3 +28,99 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+const header = document.querySelector(".header");
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+
+message.innerHTML =
+  'We use cookies fo improved functiononality and analytics. <button class="btn btn--close-cookie">Got it</button>';
+
+header.append(message);
+
+header.after(message); // as a sibling
+
+//Delete elements
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove(message);
+  });
+
+//How the DOM is really works
+
+// - Interact with the browser
+// - Create modify and delete HTML elements, set styles, classes and atributes, listen and respond to events
+//DOM is very complex API that contains lots of methods and properties
+//DOM has different types of nodes, html elements, text elements
+
+//How the DOM API is organizend behind the scenes
+
+//every node is of the type node
+
+// each node is represented by js object, this node object gets acces to spetial node methods and  properties(.textCOntent, childNOdes, parentNode, cloneNode)
+
+//Each node type has diffrent child types: Element, Text, Comment, Document
+
+//so whenever there is text element we can already know that it gets its own node <p>text<p>/ <!--Comment--> because whatever is in the code is in the DOM
+//<p></p> <!----> - are represented by Element and each element gets acces to tons of properties InnerHTML Classlist, children parentElement
+
+//Element has an inneer HTMLElement child type, and that element has execly one child type for each html element that exest in HTML: HTMLButtonElement, HTML DivElement and those elements can have unique propeerties
+
+//Iheritanse of methods and properties
+
+//any HTMLElement will have acces to .addEventListener();.cloneNode(); or closest() mehods
+
+//Document is also a node type
+
+//DOM API needs all the node-type to listen to events
+// we usually liten to events with addEventlistener on an Element or Document all of this works becase of the spectial node type called EventTarget which is the parrent of both the Node-type and also the Window node-type because all types will inherit this method
+
+//
+//Selecting, creating and deleting elements
+
+// console.log(document.documentElement); //selecitng entire document// selecting entire elemet to apply style.
+// //for these spetial elements we dont need to write any selector
+// console.log(document.head);
+// console.log(document.body);
+
+// //oherwise we can use
+// const header = document.querySelector(".header"); // selecting first element that matches
+
+// const allSections = document.querySelectorAll(".section"); // this will return a node list and that will contain all the elements that are the sections
+
+// console.log(allSections);
+
+// document.getElementById("section--1"); //here we only pass the ID name itself without the selector
+
+// const allButtons = document.getElementsByTagName("button");
+// console.log(allButtons); //here we have all the buttons returned in HTMLcollection, if DOM changes this collection is immediately UPDATED automaticaly
+
+// console.log(document.getElementsByClassName("btn")); //also returns a live elements collection
+
+// //Creating and inserting ellements
+
+// const message = document.createElement("div"); // creates a dom element and stores that element in into the message
+// message.classList.add("cookie-message");
+
+// // message.textContent =
+// // "We use cookies fo improved functiononality and analytics.";
+// message.innerHTML =
+//   'We use cookies fo improved functiononality and analytics. <button class="btn btn--close-cookie">Got it</button>'; //now we nave this element and all we have to do is to insert it in our DOM
+// // header.prepend(message); // prepending adds the element as the first child of selected element into DOM
+
+// header.append(message); // append adds the element as the last child of selected element into DOM
+
+// //The message now is only at the bottom, that means that this element is now a live element at the DOM and can not be at the multiple places at the time, so append method moved the element from being the first child to be the last child
+
+// // header.append(message.cloneNode(true)); //this allows to copy the element. here we pass in true which means that all the child elements will aslo be copied along
+
+// // header.before(message); //as a sibling
+// header.after(message); // as a sibling
+
+// //Delete elements
+// document
+//   .querySelector(".btn--close-cookie")
+//   .addEventListener("click", function () {
+//     message.remove(message);
+//     // message.parentElement.removeChild(message);//before remove method was introduced
+//   });
